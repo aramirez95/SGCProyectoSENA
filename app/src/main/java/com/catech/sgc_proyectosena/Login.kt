2dceac39dd370da5f474d01ce0762ca0cfcd3c05
Andres Fernando Ramirez <52538470+aramirez95@users.noreply.github.com>
2025-03-15 17:16:50 -0500

    Initial commit

---
+++ app/src/main/java/com/catech/sgc_proyectosena/Login.kt
@@ -0,0 +1,26 @@
+package com.catech.sgc_proyectosena
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun iniciar(view: View){
+        val inicio = Intent(this,Inicio::class.java)
+        startActivity(inicio)
+    }
+}
